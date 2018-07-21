87f0cdc0374a01bd1637e5b8f914e58df6a5f32e
ggeter <ggeter@gmail.com>
2018-07-21 12:20:17 -0500

    maybe this is the last try at commit/pub flow?

--- public/client.js
+++ public/client.js
@@ -4,23 +4,10 @@
 console.log('NetWorth has started.');
 
 function NetWorthApp() {
-
   // variables, setup, shared functions
-  // grab template HTML
-  var itemDiv = document.getElementById('tmpl_itemDiv').innerHTML;
-  var itemListDiv = document.getElementById('item-list');
- 
-  // this is the template engine test. Results logged to console on app start
-  console.log(tmplrender("<div>{{TESTHOLDER}}</div><span id='{{SPANID}}'</span>", {
-    TESTHOLDER: "this is the DIV innerHTML.",
-    SPANID: "this-is-the-span-id"
-  }));
 
-  
   
   // application
-  
-  
   
 }
 
