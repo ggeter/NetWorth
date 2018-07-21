8c3609e86147934ad5400b9c128fa7d460b70e79
ggeter <ggeter@gmail.com>
2018-07-21 11:43:16 -0500

    template test

--- public/client.js
+++ public/client.js
@@ -24,9 +24,29 @@ function NetWorthApp() {
   
 }
 
-function QuickTemplate () {
- 
-  
+function tmplrender(tmplDATA, tmplID, destID)  {
+
+  //simple replace string templating
+
+  //grab template code
+  var tmplHTML = document.getElementById(tmplID).innerHTML;
+
+  //grab destination node
+  var destNode = destID ? document.getElementById(destID) : false;
+
+  //remove comment tokens first 
+  tmplHTML = tmplHTML.split('/*').join('').split('*/').join('');
+
+  // in my research over the years, split/join is suprisingly performant
+  for (var p in tmplDATA) {
+    tmplHTML = tmplHTML.split('{{' + p + '}}').join(tmplDATA[p]);
+  }
+
+  if (destNode) {
+    destNode.innerHTML = tmplHTML;
+  }
+
+  return tmplHTML;
 }
 
 function DOMReady(fn) {
