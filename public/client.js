420f755ef70222cb272de0f54cc6961649e01ae1
Glitch (networth) <none>
2018-07-22 01:15:46 +0000

    🎉🎁 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -7,45 +7,55 @@ function NetWorthApp() {
 
   
   // application
-var chart = new Chartist.Line('.ct-chart', {
-  series: [
-    {
+  var chart = new Chartist.Line('.ct-chart', {
+    labels: ['1', '2', '3', '4', '5', '6', '7', '8'],
+    // Naming the series with the series object array notation
+    series: [{
       name: 'series-1',
-      data: [
-        {x: new Date("1/1/2018"), y: 73},
-        {x: new Date("1/1/2019"), y: 20},
-        {x: new Date("1/1/2020"), y: 45},
-        {x: new Date("1/1/2021"), y: 40},
-        {x: new Date("1/1/2022"), y: 20},
-        {x: new Date("1/1/2023"), y: 32},
-        {x: new Date("1/1/2024"), y: 18},
-        {x: new Date("1/1/2025"), y: 11}
-      ]
-    },
-    {
+      data: [5, 2, -4, 2, 0, -2, 5, -3]
+    }, {
       name: 'series-2',
-      data: [
-        {x: new Date("1/1/2018"), y: 53},
-        {x: new Date("1/1/2019"), y: 40},
-        {x: new Date("1/1/2020"), y: 45},        
-        {x: new Date("1/1/2021"), y: 53},
-        {x: new Date("1/1/2022"), y: 35},
-        {x: new Date("1/1/2023"), y: 30},
-        {x: new Date("1/1/2024"), y: 30},
-        {x: new Date("1/1/2025"), y: 10}
-      ]
+      data: [4, 3, 5, 3, 1, 3, 6, 4]
+    }, {
+      name: 'series-3',
+      data: [2, 4, 3, 1, 4, 5, 3, 2]
+    }]
+  }, {
+    fullWidth: true,
+    // Within the series options you can use the series names
+    // to specify configuration that will only be used for the
+    // specific series.
+    series: {
+      'series-1': {
+        lineSmooth: Chartist.Interpolation.step()
+      },
+      'series-2': {
+        lineSmooth: Chartist.Interpolation.simple(),
+        showArea: true
+      },
+      'series-3': {
+        showPoint: false
+      }
     }
-  ]
-}, {
-  showPoint: false,
-  axisX: {
-    type: Chartist.FixedScaleAxis,
-    divisor: 8,
-    labelInterpolationFnc: function(value) {
-      return moment(value).format('YYYY');
-    }
-  }
-});
+  }, [
+    // You can even use responsive configuration overrides to
+    // customize your series configuration even further!
+    ['screen and (max-width: 320px)', {
+      series: {
+        'series-1': {
+          lineSmooth: Chartist.Interpolation.none()
+        },
+        'series-2': {
+          lineSmooth: Chartist.Interpolation.none(),
+          showArea: false
+        },
+        'series-3': {
+          lineSmooth: Chartist.Interpolation.none(),
+          showPoint: true
+        }
+      }
+    }]
+  ]);
 
 }
   
