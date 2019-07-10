157027bf732314770dbae21b295669f3ec566ee6
ggeter <2258225+ggeter@users.noreply.github.com>
2019-07-09 21:10:31 -0500

    added ChartJS sample to begin conversion

--- public/client.js
+++ public/client.js
@@ -61,7 +61,7 @@ function runScenario() {
     var sdata = sd;    
     // compute time-series DR/CR per item
     
-    var currentScenario = sdata["S001"];
+    var currentScenario = sdata["S002"];
     
     var yearsToAnalyze =  currentScenario.BASELINE.yearstoretirement +
                           currentScenario.BASELINE.yearspastretirement;
@@ -285,7 +285,7 @@ function DOMReady(fn) {
 
 function getScenarios(callback) {
   var request = new XMLHttpRequest();
-  request.open('GET', 'sampleScenarios.json', true);
+  request.open('GET', 'public/sampleScenarios.json', true);
 
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
@@ -306,3 +306,94 @@ function getScenarios(callback) {
   request.send();
   
 }
+
+function randomScalingFactor() {
+  return parseInt( Math.random() * 100);
+}
+
+var config = {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'Unfilled',
+      fill: false,
+      backgroundColor: window.chartColors.blue,
+      borderColor: window.chartColors.blue,
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ],
+    }, {
+      label: 'Dashed',
+      fill: false,
+      backgroundColor: window.chartColors.green,
+      borderColor: window.chartColors.green,
+      borderDash: [5, 5],
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ],
+    }, {
+      label: 'Filled',
+      backgroundColor: window.chartColors.red,
+      borderColor: window.chartColors.red,
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ],
+      fill: true,
+    }]
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart'
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
+    hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Month'
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Value'
+        }
+      }]
+    }
+  }
+};
+
+window.onload = function() {
+  var ctx = document.getElementById('chartcanvas').getContext('2d');
+  window.myLine = new Chart(ctx, config);
+};
