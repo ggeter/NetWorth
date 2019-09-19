a809a0e577cceef6d7371852bd3d04553eedbcb8
ggeter <2258225+ggeter@users.noreply.github.com>
2019-09-19 14:21:07 -0500

    Finally back from deletion disaster...

--- api/scenarios/[scenario].js
+++ api/scenarios/[scenario].js
@@ -3,5 +3,21 @@ export default (req, res) => {
 		query: { scenario }
 	} = req;
 
-	res.send(`Hello ${scenario}!`);
+	var faunadb = require('faunadb'),
+		q = faunadb.query;
+
+	var client = new faunadb.Client({
+		secret: process.env.FAUNA_DB_ADMIN_KEY_001
+	});
+
+	client
+		.query(q.Get(q.Match(q.Index('scenario_by_scenarioname'), scenario)))
+		.then(function(ret) {
+			console.log(ret);
+			res.send(ret);
+		})
+		.catch(function(ret) {
+			console.log('No Data In Index!');
+			res.send(ret);
+		});
 };
